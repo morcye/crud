da1a48098fd8acd8aa2262f34a571e5c05883bcb
morcye <paulohenriquealvesdutra@gmail.com>
2021-12-20 01:10:36 -0300

    Primeiro commit

--- src/main/java/com/paulo/senai/domain/Cliente.java
+++ src/main/java/com/paulo/senai/domain/Cliente.java
@@ -41,7 +41,20 @@ public class Cliente implements Serializable {
 	
 	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
-
+	
+	public double somar(double num , double num2) {
+		return num + num2;
+	}
+		
+	public double subtrair(double num , double num2) {
+			return num - num2;
+	}
+	public double mutiplicar(double num , double num2) {
+		return num * num2;
+	}
+	public double dividir(double num , double num2) {
+		return num / num2;
+	}
 	public Cliente() {
 	}
 
